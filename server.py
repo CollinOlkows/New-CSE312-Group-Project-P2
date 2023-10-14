132be271e9a301d08d753d7d0e4843182c72a836
Collin <99486132+CollinOlkows@users.noreply.github.com>
2023-10-14 19:05:04 -0400

    setting up paths that will need to get done

--- server.py
+++ server.py
@@ -1,12 +1,39 @@
-from flask import Flask,make_response
+from flask import Flask,make_response,send_from_directory,render_template,request,redirect,url_for
+import databaseutils
+import bcrypt
+import html
+import secrets
+import datetime
 
 
 #File for all the server stuff
 
 app = Flask(__name__)
 
+@app.route('/<path:path>')
+def send_static(path):
+    response = make_response(send_from_directory('static', path),200)
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
 @app.route('/')
 def index():
+    return make_response('Home Page',200)
+
+@app.route('/login')
+def login():
+    return make_response('Home Page',200)
+
+@app.route('/signup')
+def signup():
+    return make_response('Home Page',200)
+
+@app.route('/post')
+def post():
+    return make_response('Home Page',200)
+
+@app.route('/feed')
+def feed():
     return make_response('Home Page',200)
 
 
