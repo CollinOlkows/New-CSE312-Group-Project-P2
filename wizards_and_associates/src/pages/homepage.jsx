4991fe4431eab176ab0732756cf07a5ead1a8a82
TheGarten <williambizier21@gmail.com>
2023-10-21 02:56:06 -0400

    working on da home page

--- wizards_and_associates/src/pages/homepage.jsx
+++ wizards_and_associates/src/pages/homepage.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import '../App.css';
+import '../styles/assets/homelogo.css'
+import logo from '../assets/images/photoshopped_logo/logo.png';
 
 function Homepage() {
   return (
-    <div className="homepage">
-      <header>
-        <h1>Welcome Wizards</h1>
-      </header>
-      <main>
-        <p>Together we can spread spells and misinformation.</p>
-      </main>
-    
-      <img
-        src={'./assets/images/photoshopped_logo/logo.png'}
-        alt="" // Empty string for decorative images
-        style={{
-            width: '1000px',
-            height: '1000px',
-            border: '1px solid #000',
-        }}
-        />
-
+    <div>
+      <img src={logo} alt="Logo" className="logo"/>
     </div>
   );
 }
