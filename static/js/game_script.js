5912fd414870f6b864477a8c2b0e87d326f5feec
Collin <99486132+CollinOlkows@users.noreply.github.com>
2023-12-15 06:50:36 -0500

    loads an image

--- static/js/game_script.js
+++ static/js/game_script.js
@@ -1,33 +1,35 @@
 
 
 
-function start_game(players,judge, user){
-    game_string = '<h1>Scoreboard</h1><div id="user_list">'
+function start_game(players,judge, user,img){
+    ply = true
+    if(judge == user){
+        game_string = '<div id="prompt_div" class="GIPprompt"><h2 class="GIPpromptTitle">You are the judge. Please wait for all players to submit</h2></div><div class="GIPgameContainer"><div class="GIPscoreBoard"><h1>SCOREBOARD</h1><div class="GIPscoreBoardContainer">'
+        ply = false
+    }else{
+    game_string = '<div id="prompt_div" class="GIPprompt"><h2 class="GIPpromptTitle">TYPE YOUR PROMPT BELOW</h2><textarea id="promptInput" class="GIPpromptForm" maxlength="300" placeholder="Prompt"></textarea><button class="GIPpromptSubmit" id="prompt_submit">SUBMIT YOUR PROMPT</button></div><div class="GIPgameContainer"><div class="GIPscoreBoard"><h1>SCOREBOARD</h1><div class="GIPscoreBoardContainer"><div id="user_list">'
+    }
     let i = 0;
-
     while (i < players.length) {
         if(players[i]['username']==judge){
-            game_string += '<p id='+ players[i]['username'] + '>' + '⚖️'+ players[i]['username'] + ': 0</p>'
+            game_string += '<p id='+ players[i]['username'] + '>' + '⚖️'+ players[i]['username'] + ': '+String(players[i]['score'])+'</p>'
         }else{
-            game_string += '<p id='+ players[i]['username'] + '>' + '🔴'+ players[i]['username'] + ': 0</p>'
+            game_string += '<p id='+ players[i]['username'] + '>' + '🔴'+ players[i]['username'] + ': '+String(players[i]['score'])+'</p>'
         }
         i++;
     }
-    if(judge == user){
-        game_string += '<h1 id=' + judge + '>' + judge + ' is Judging!</h1><img src="/catalog/logo.png" style="height: 5vmax; width: 5vmax;"></div><div id="prompt_div"><h2>You are the judge, please wait for the other players to submit!</h2></div>'
-    }else{
-        game_string += '<h1 id=' + judge + '>' + judge + ' is Judging!</h1><img src="/catalog/logo.png" style="height: 5vmax; width: 5vmax;"></div><div id="prompt_div"><h2>Type Your Prompt Below!</h2><input type="text" placeholder="Prompt" id="prompt"><button onclick="submit_prompt()">Submit Prompt!</button></div>'
-    } 
+    game_string+='</div></div></div><div class="GIPjudge"><h1>JUDGING</h1><div class="GIPjudgeContainer"><h2 id="judge_name">CockusBallus</h2><img id="current_image" src="/static/images/lebronDuck.jpeg" class="GIPjudgeImg"></div></div></div>'
 //document.querySelector('html').innerHTML = '<html><head><link  rel="stylesheet" href="/css/style.css"><link  rel="stylesheet" href="/css/lobby.css"><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.0.4/socket.io.js" integrity="sha512-aMGMvNYu8Ue4G+fHa359jcPb1u+ytAF+P2SCb+PxrjCdO3n3ZTxJ30zuH39rimUggmTwmh2u7wvQsDTHESnmfQ==" crossorigin="anonymous"></script></head>  <body><div id="Game_Board"><h1>Scoreboard</h1><div id="user_list"><p>🟢William: 0</p><p>🔴Joe: 0</p><p>⚖️Johnny: 0</p><p>🔴Daniel: 0</p><p>🔴Rob: 0</p></div><div><h1>Johnny is Judging!</h1><img src="/catalog/logo.png" style="height: 5vmax; width: 5vmax;"></div><div><h2>Type Your Prompt Below!</h2><input type="text" placeholder="Prompt"><button>Submit Prompt!</button></div></div></body></html>';
 document.getElementById('Game_Board').innerHTML = game_string
+if(ply){
+    document.getElementById('prompt_submit').addEventListener("click", submit_the_prompt);
+}
+document.getElementById('judge_name').innerHTML=judge
+document.getElementById('current_image').src =img
 
 }
 
-function submit_prompt(user){
-    let prompt = document.getElementById('prompt').value;
-    document.getElementById('prompt_div').innerHTML = '<button onclick="judge_prompts();">Judging Screen</button><button onclick="Please_Wait();">Wait</button>'
-    document.getElementById('Joe').innerHTML='🟢Joe: 0'
-}
+
 
 function judge_prompts(){
     document.getElementById('Game_Board').innerHTML = '<h1>Scoreboard</h1><div id="user_list"><p>🟢William: 0</p><p>🟢Joe: 0</p><p>⚖️Johnny: 0</p><p>🟢Daniel: 0</p><p>🟢Rob: 0</p></div><div id ="image"><h1>Johnny is Judging!</h1><img src="/catalog/logo.png" style="height: 5vmax; width: 5vmax;"></div><div id="Prompt"><button onclick="display_scoreboard()">Chicken</button><button onclick="display_scoreboard()">Prompt2</button><button onclick="display_scoreboard()">William Bozo</button><button onclick="display_scoreboard()">IDk stuff</button><button onclick="display_scoreboard()">Other Stuff</button></div></div>'
